59be2ce7c969526415b635bc7a0e7771bf629b7b
ggorotiza1 <tabletgorotiza@gmail.com>
2024-10-25 00:46:55 -0500

    new code.5

--- sql/server.js
+++ sql/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configura la conexión a PostgreSQL
 const pool = new Pool({
