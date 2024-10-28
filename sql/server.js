c77d331deef1946ce560f721062edb21fda3ddcd
ggorotiza1 <tabletgorotiza@gmail.com>
2024-10-27 20:49:53 -0500

    gestion_factura

--- sql/server.js
+++ sql/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
 
 // Configura la conexión a PostgreSQL
 const pool = new Pool({
@@ -18,6 +20,12 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', // Permitir solo este origen
+  allowedHeaders: ['Content-Type', 'Authorization'], // Permitir estos encabezados
+}));
+
 
 // Ruta para obtener los productos
 app.get('/productos', async (req, res) => {
@@ -139,6 +147,105 @@ app.post('/actualizar-stock', async (req, res) => {
   } catch (error) {
     console.error('Error al actualizar el stock:', error);
     return res.status(500).json({ mensaje: 'Error al actualizar el stock. Intenta nuevamente.' });
+  }
+});
+
+// Ruta para almacenar facturas
+app.post('/facturas', async (req, res) => {
+  const { userId, productos } = req.body;
+
+  if (!userId || !productos || productos.length === 0) {
+    return res.status(400).send('Se requieren userId y productos.');
+  }
+
+  const client = await pool.connect();
+
+  try {
+    await client.query('BEGIN');
+    const totalFactura = productos.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0);
+    const facturaResult = await client.query(
+      'INSERT INTO facturas (user_id, total) VALUES ($1, $2) RETURNING factura_id',
+      [userId, totalFactura]
+    );
+
+    const facturaId = facturaResult.rows[0].factura_id;
+
+    for (const producto of productos) {
+      await client.query(
+        'INSERT INTO detalle_factura (factura_id, producto_id, nombre_producto, cantidad, precio_unitario) VALUES ($1, $2, $3, $4, $5)',
+        [facturaId, producto.id, producto.nombre, producto.cantidad, producto.precio]
+      );
+    }
+
+    await client.query('COMMIT'); 
+    res.status(201).json({ mensaje: 'Factura almacenada con éxito.', facturaId });
+  } catch (err) {
+    await client.query('ROLLBACK'); 
+    console.error('Error al almacenar la factura:', err);
+    res.status(500).send('Error al almacenar la factura');
+  } finally {
+    client.release();
+  }
+});
+
+app.get('/ver-facturas', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM facturas ORDER BY factura_id ASC');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error al obtener facturas', error);
+    res.status(500).json({ error: 'Error al obtener las facturas' });
+  }
+});
+
+app.get('/ver-factura/:id', async (req, res) => {
+  const facturaId = parseInt(req.params.id);
+
+  try {
+    const result = await pool.query(`SELECT 
+    f.factura_id,
+    f.fecha,
+    f.total,
+    f.estado,
+    u.user_id,
+    u.user_cedula,
+    u.user_nombre,
+    u.user_apellido,
+    u.user_correo,
+    u."user_foto-url",
+    u.user_rol,
+    u.user_estado,
+    json_agg(
+        json_build_object(
+            'producto_id', d.producto_id,
+            'nombre_producto', d.nombre_producto,
+            'cantidad', d.cantidad,
+            'precio_unitario', d.precio_unitario,
+            'subtotal', d.subtotal
+        )
+    ) AS productos
+FROM 
+    facturas f
+JOIN 
+    usuario u ON f.user_id = u.user_id
+JOIN 
+    detalle_factura d ON f.factura_id = d.factura_id
+WHERE 
+    f.factura_id = $1 -- Aquí se reemplaza $1 con el ID específico de la factura que deseas obtener
+GROUP BY 
+    f.factura_id, u.user_id;
+
+`, [facturaId]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Factura no encontrada' });
+    }
+
+
+    res.json(result.rows[0]); 
+  } catch (error) {
+    console.error('Error al obtener la factura:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
 
